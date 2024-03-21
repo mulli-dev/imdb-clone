0179af81a5d9c3072ae314d6d8a92f7452658c25
Jacob Mulli <jacobmullie2@gmail.com>
2024-03-21 13:13:57 +0300

    all added today

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <img className="h-52" src="spinner.svg" alt="loading..." />
+      <Image className="h-52" src="spinner.svg" alt="loading..." />
     </div>
   );
 }
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -10,7 +10,7 @@ export default async function MoviePage({ params }) {
   return (
     <div className="w-full">
       <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
-        <img
+        <Image
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
           }`}
